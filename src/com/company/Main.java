57fdf67e1627f194d60fdabfe88e3cabfa21318f
mi_iroha <chimosy21@gmail.com>
2021-02-17 14:59:41 +0900

    error: Firebaseの機能を使おうとするとコンパイルエラーが出る

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // 送信リクエストを認証する
-        
-
-
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
+                .build();
+        FirebaseApp.initializeApp(options);
     }
 }
