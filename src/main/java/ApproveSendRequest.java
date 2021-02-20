f3ccd5d5732197cc57cf7a04bf89898c388930a5
mi_iroha <chimosy21@gmail.com>
2021-02-20 13:04:01 +0900

    コードレビューの指摘事項修正

--- src/main/java/ApproveSendRequest.java
+++ src/main/java/ApproveSendRequest.java
@@ -2,9 +2,13 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import java.io.IOException;
+
 public class ApproveSendRequest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 送信リクエストを認証する
+        
+        // SDKを初期化
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.getApplicationDefault())
                 .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
